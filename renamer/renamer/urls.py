8c2a2dc9a0e1c8b029f4e8bf5f7656e10036c165
Robert Casties <casties@mpiwg-berlin.mpg.de>
2022-01-04 21:41:26 +0100

    change config to run behind traefik.

--- renamer/renamer/urls.py
+++ renamer/renamer/urls.py
@@ -7,27 +7,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('imageadmin/', include('imageadmin.urls')),
-    path('admin/', admin.site.urls),
+    path('imageadmin/admin/', admin.site.urls),
 ]
-
-#base_urlpatterns = []
-#if 'django.contrib.admin' in settings.INSTALLED_APPS:
-#    base_urlpatterns += patterns('',
-#        url(r'^admin/', include(admin.site.urls))
-#    )
-
-#base_urlpatterns += patterns('renamer.views.main',
-#     url(r'^$', 'home', name="home"),
-#     url(r'^manage/*$', 'manage', name="manage"),
-#     url(r'^to_archive/*$', 'to_archive'),
-#     url(r'^to_diva/*$', 'to_diva'),
-#     url(r'^diva_redo/*$', 'diva_redo'),
-#     url(r'^login/*$', 'user_login'),
-#     url(r'^logout/*$', 'user_logout')
-# )
-#
-# urlpatterns = patterns('',
-#         url(r'^imageadmin/', include(base_urlpatterns))
-#     )
-#
-# urlpatterns += staticfiles_urlpatterns()
